b7f8fa38207c292dc03ed0e61cb89af3ee06cfad
Amit-Shaw7 <maahiamit777@gmail.com>
2023-12-21 19:40:42 +0530

    week 3 middlewares done

--- week-3/01-middlewares/02-ratelimitter.js
+++ week-3/01-middlewares/02-ratelimitter.js
@@ -12,9 +12,25 @@ const app = express();
 // clears every one second
 
 let numberOfRequestsForUser = {};
+
 setInterval(() => {
     numberOfRequestsForUser = {};
 }, 1000)
+
+app.use(function(req , res ,next) {
+  const userId = req.headers["user-id"];
+
+  if(numberOfRequestsForUser[userId]){
+    numberOfRequestsForUser[userId] += 1;
+    if(numberOfRequestsForUser[userId] > 5){
+      return res.status(404).send("no entry");
+    }
+    next();
+  }else{
+    numberOfRequestsForUser[userId] = 1;
+    next();
+  }
+})
 
 app.get('/user', function(req, res) {
   res.status(200).json({ name: 'john' });
@@ -23,5 +39,6 @@ app.get('/user', function(req, res) {
 app.post('/user', function(req, res) {
   res.status(200).json({ msg: 'created dummy user' });
 });
+
 
 module.exports = app;
