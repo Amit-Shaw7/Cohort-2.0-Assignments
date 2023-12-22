ba37b3dd3546224134e242917422241425217348
Amit-Shaw7 <maahiamit777@gmail.com>
2023-12-22 19:55:22 +0530

    week 3 jwt done

--- week-3/02-jwt/index.js
+++ week-3/02-jwt/index.js
@@ -1,5 +1,6 @@
 const jwt = require('jsonwebtoken');
 const jwtPassword = 'secret';
+const zod = require('zod');
 
 
 /**
@@ -13,8 +14,21 @@ const jwtPassword = 'secret';
  *                        Returns null if the username is not a valid email or
  *                        the password does not meet the length requirement.
  */
+
+const emailSchema = zod.string().email();
+const passwordSchema = zod.string().min(6);
+
 function signJwt(username, password) {
     // Your code here
+    const validEmail = emailSchema.safeParse(username);
+    const validPassword = passwordSchema.safeParse(password);
+
+    if (!validEmail.success || !validPassword.success) {
+        return null;
+    }
+
+    const token = jwt.sign({ username }, jwtPassword);
+    return token;
 }
 
 /**
@@ -27,6 +41,16 @@ function signJwt(username, password) {
  */
 function verifyJwt(token) {
     // Your code here
+    if (!token) {
+        return false;
+    }
+
+    try {
+        jwt.verify(token, jwtPassword); // throws an exception
+        return true;
+    } catch (error) {
+        return false;
+    }
 }
 
 /**
@@ -38,12 +62,21 @@ function verifyJwt(token) {
  */
 function decodeJwt(token) {
     // Your code here
+    if (!token) {
+        return false;
+    }
+
+    const decoded = jwt.decode(token);
+    if (!decoded) {
+        return false;
+    }
+    return true;
 }
 
 
 module.exports = {
-  signJwt,
-  verifyJwt,
-  decodeJwt,
-  jwtPassword,
+    signJwt,
+    verifyJwt,
+    decodeJwt,
+    jwtPassword,
 };
